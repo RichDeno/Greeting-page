6d87c2688e5f017ad5008c5d66a9638985c1372e
Denen Desmond Amua <amuadesmond1@gmail.com>
2024-11-18 20:54:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Prompt the user to enter their name
+var username = prompt("Please enter your name:");
+
+// Display the greeting in an alert
+alert("Hello " + username + "!");
+
+// Write the greeting in <h1> tags to the document
+document.write("<h1>Hello " + username + "!</h1>");
